bc1af64cbfdcae736c8a26e64e403f2b54b3130e
Daniel Whiffing <danielwhiffing@gmail.com>
2020-03-17 23:19:28 -0400

    Refactors

--- server/schema/Card.ts
+++ server/schema/Card.ts
@@ -1,11 +1,15 @@
 import { type, Schema } from '@colyseus/schema'
+
 export class Card extends Schema {
   @type('number')
   value: number
+
   @type('number')
   suit: number
+
   @type('number')
   index: number
+
   constructor(value: number, suit: number, index: number) {
     super()
     this.value = value
